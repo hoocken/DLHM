Fixed fem by using stabler R calculation (Shape matching)

src/soft_body/create_tetrahedral_mesh.py
@@ -1,6 +1,7 @@
 # Downsample the mesh
 # Create tetrahedral mesh from semgentation
 # Compose them all 
+import numpy as np
 import pyvista as pv
 import trimesh
 import pytetwild
@@ -21,7 +22,33 @@ class Tetrahedralize():
         self.lean_mesh_path = lean_mesh_path
         self.tet_mesh: pv.UnstructuredGrid | None = None
 
+    def combine_mesh(self, mesh1_path, mesh2_path):
+        mesh1 = trimesh.load(mesh1_path)
+        # Mesh 2: The inner mesh to be subtracted (creating a cavity)
+        mesh2 = trimesh.load(mesh2_path)
+        target_faces = 5000
+        current_faces = len(mesh2.faces)
+
+        if current_faces > target_faces:
+            # Calculate the fraction needed to hit the target
+            percent_needed = target_faces / current_faces
+            downsampled_mesh = mesh2.simplify_quadric_decimation(percent=1 - percent_needed)
+        else:
+            print("Mesh already has fewer faces than the target.")
+
+        print(f"Mesh 1 (Outer): {len(mesh1.faces)} faces")
+        print(f"Mesh 2 (Inner): {len(downsampled_mesh.faces)} faces")
+
+        # Ensure both meshes are clean and watertight for the boolean operation
+        mesh1.fill_holes()
+        downsampled_mesh.fill_holes()
+
+        print("Combining mesh...")
+        combined_mesh = trimesh.util.concatenate([mesh1, downsampled_mesh])
+        return combined_mesh
+
     def create_tetrahedra_mesh(self):
+        # mesh = self.combine_mesh(self.smpl_path, self.lean_mesh_path)
         mesh = pv.read(self.smpl_path)
 
         # Triangulate it to extract raw vertex and face matrices
@@ -32,10 +59,26 @@ class Tetrahedralize():
         # PyVista face arrays look like: [3, v0, v1, v2, 3, v0, v1, v2, ...]
         faces = tri_mesh.faces.reshape(-1, 4)[:, 1:]
 
+        # tin = pymeshfix.PyTMesh()
+        # tin.load_array(vertices, faces)
+        # tin.clean(max_iters=10, inner_loops=3)  # Removes self-intersections & seals mesh
+
+        # cleaned_mesh = pv.PolyData(tin.return_points(), np.hstack([np.full((tin.return_faces().shape[0], 1), 3), tin.return_faces()]))
+
+
+
         # Generate the tetrahedral mesh (pytetwild handles the self-intersections)
         print("Fixing self-intersections and generating volume mesh...")
-        tet_vertices, tet_cells = pytetwild.tetrahedralize(vertices, faces)
+        # tgen = tetgen.TetGen(cleaned_mesh)
+        tet_vertices, tet_cells = pytetwild.tetrahedralize(vertices, faces, edge_length_abs=0.2)
 
+        # tgen.tetrahedralize(
+        #     quality=False,
+        #     mindihedral=10.0,
+        #     minratio=1.5,
+        #     maxvolume=0.1
+        # )
+        # tet_mesh = tgen.grid
         tet_mesh = pv.UnstructuredGrid({pv.CellType.TETRA: tet_cells}, tet_vertices)
 
         print(f"Success! Generated {tet_mesh.n_cells} tetrahedra.")
@@ -45,6 +88,7 @@ class Tetrahedralize():
         adipose_mesh = trimesh.load(self.adipose_mesh_path)
 
         # Ensure the boundary mesh is closed and watertight for accurate queries
+        print("Filling holes for boundary mesh...")
         lean_mesh.fill_holes()
         adipose_mesh.fill_holes()
         
@@ -62,6 +106,7 @@ class Tetrahedralize():
     def _calculate_inside_points(self, nodes, mesh):
         # Calculate signed distance: negative values are INSIDE, positive are OUTSIDE
         # (A small tolerance ensures nodes exactly on the surface aren't lost)
+        print("Calculating inside points...")
         proximity_engine = trimesh.proximity.ProximityQuery(mesh)
         signed_distances = proximity_engine.signed_distance(nodes)
         mask = signed_distances > -1e-3
@@ -107,4 +152,4 @@ class Tetrahedralize():
 if __name__ == "__main__":
     tet = Tetrahedralize("outputs/hit_best/smpl_mesh.obj", "outputs/hit_best/AT_mesh.obj", "outputs/hit_best/LT_mesh.obj")
     tet.create_tetrahedra_mesh()
-    tet.plot()
+    # tet.plot()