d4fa83652ffd57420bf3443b5d2192f2300489aa
go34dij <go34dij@mytum.de>
2026-07-27 18:55:28 +0200

    Clean up

--- src/soft_body/create_tetrahedral_mesh.py
+++ src/soft_body/create_tetrahedral_mesh.py
@@ -1,16 +1,21 @@
 # Downsample the mesh
 # Create tetrahedral mesh from semgentation
 # Compose them all 
+import os
+import pickle
+
 import numpy as np
 import pyvista as pv
 import trimesh
 import pytetwild
+import tetgen
+import pymeshfix
 
 class Tetrahedralize():
     """
-    Create a volumetric tetrahedralized mesh with marked nodes that is inside the lean and adipose tissue. 
+    Create a volumetric tetrahedralized mesh. 
     """
-    def __init__(self, smpl_path: str, adipose_mesh_path: str, lean_mesh_path: str):
+    def __init__(self, smpl_path: str):
         """
         Parameters:
             smpl_path: Path to the obj file of SMPL
@@ -18,9 +23,9 @@ class Tetrahedralize():
             lean_mesh_path: Path to the calculated lean tissue mesh
         """
         self.smpl_path = smpl_path
-        self.adipose_mesh_path = adipose_mesh_path
-        self.lean_mesh_path = lean_mesh_path
         self.tet_mesh: pv.UnstructuredGrid | None = None
+
+        self.out_folder = 'outputs/tet_mesh'
 
     def combine_mesh(self, mesh1_path, mesh2_path):
         mesh1 = trimesh.load(mesh1_path)
@@ -59,49 +64,56 @@ class Tetrahedralize():
         # PyVista face arrays look like: [3, v0, v1, v2, 3, v0, v1, v2, ...]
         faces = tri_mesh.faces.reshape(-1, 4)[:, 1:]
 
-        # tin = pymeshfix.PyTMesh()
-        # tin.load_array(vertices, faces)
-        # tin.clean(max_iters=10, inner_loops=3)  # Removes self-intersections & seals mesh
+        tin = pymeshfix.PyTMesh()
+        tin.load_array(vertices, faces)
+        tin.clean(max_iters=10, inner_loops=3)  # Removes self-intersections & seals mesh
 
-        # cleaned_mesh = pv.PolyData(tin.return_points(), np.hstack([np.full((tin.return_faces().shape[0], 1), 3), tin.return_faces()]))
+        cleaned_mesh = pv.PolyData(tin.return_points(), np.hstack([np.full((tin.return_faces().shape[0], 1), 3), tin.return_faces()]))
 
 
 
         # Generate the tetrahedral mesh (pytetwild handles the self-intersections)
         print("Fixing self-intersections and generating volume mesh...")
-        # tgen = tetgen.TetGen(cleaned_mesh)
-        tet_vertices, tet_cells = pytetwild.tetrahedralize(vertices, faces, edge_length_abs=0.2)
+        tgen = tetgen.TetGen(cleaned_mesh)
+        # tet_vertices, tet_cells = pytetwild.tetrahedralize(vertices, faces, edge_length_abs=0.2)
 
-        # tgen.tetrahedralize(
-        #     quality=False,
-        #     mindihedral=10.0,
-        #     minratio=1.5,
-        #     maxvolume=0.1
-        # )
-        # tet_mesh = tgen.grid
-        tet_mesh = pv.UnstructuredGrid({pv.CellType.TETRA: tet_cells}, tet_vertices)
+        tgen.tetrahedralize(
+            mindihedral=10.0,
+            minratio=1.5,
+        )
+        tet_mesh = tgen.grid
+        # tet_mesh = pv.UnstructuredGrid({pv.CellType.TETRA: tet_cells}, tet_vertices)
 
         print(f"Success! Generated {tet_mesh.n_cells} tetrahedra.")
         tet_nodes = tet_mesh.points  # Extract all (N, 3) node coordinates
 
-        lean_mesh = trimesh.load(self.lean_mesh_path)
-        adipose_mesh = trimesh.load(self.adipose_mesh_path)
+        # lean_mesh = trimesh.load(self.lean_mesh_path)
+        # adipose_mesh = trimesh.load(self.adipose_mesh_path)
 
         # Ensure the boundary mesh is closed and watertight for accurate queries
-        print("Filling holes for boundary mesh...")
-        lean_mesh.fill_holes()
-        adipose_mesh.fill_holes()
+        # print("Filling holes for boundary mesh...")
+        # lean_mesh.fill_holes()
+        # adipose_mesh.fill_holes()
         
-        lean_mask = self._calculate_inside_points(tet_nodes, lean_mesh)
-        tet_mesh.point_data["is_lean"] = lean_mask
+        # lean_mask = self._calculate_inside_points(tet_nodes, lean_mesh)
+        # tet_mesh.point_data["is_lean"] = lean_mask
 
-        adipose_mask = self._calculate_inside_points(tet_nodes, adipose_mesh)
-        tet_mesh.point_data["is_adipose"] = adipose_mask
+        # adipose_mask = self._calculate_inside_points(tet_nodes, adipose_mesh)
+        # tet_mesh.point_data["is_adipose"] = adipose_mask
 
         print("Finished generation of tetrahedralized mesh with marked nodes!")
 
         self.tet_mesh = tet_mesh
-        self.tet_mesh.save("mesh.vtu")
+        self.tet_mesh.save(f'{self.out_folder}/tet_mesh.vtu')
+        # pcd = pv.PolyData(self.tet_mesh.points)
+        # pcd.save(f'{self.out_folder}/tet_mesh.ply', binary=True)
+
+        # tetrahedra = tet_mesh.extract_cells_by_type(pv.CellType.TETRA).cells.reshape(-1, 5)[:, 1:]
+
+        # data_dict = { "points": self.tet_mesh.points, "tetrahedra": tetrahedra}
+
+        # with open(f'{self.out_folder}/tet_mesh.pkl', 'wb') as f:
+        #     pickle.dump(data_dict, f)
 
     def _calculate_inside_points(self, nodes, mesh):
         # Calculate signed distance: negative values are INSIDE, positive are OUTSIDE
@@ -150,6 +162,6 @@ class Tetrahedralize():
 
 
 if __name__ == "__main__":
-    tet = Tetrahedralize("outputs/hit_best/smpl_mesh.obj", "outputs/hit_best/AT_mesh.obj", "outputs/hit_best/LT_mesh.obj")
+    tet = Tetrahedralize("outputs/motion/smpl_mesh.obj")
     tet.create_tetrahedra_mesh()
     # tet.plot()
